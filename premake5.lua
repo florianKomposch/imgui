fe451c696c3d242d033f305e9845a1f9fd6d385c
Florian Komposch <florian.komposch@gmail.com>
2024-12-01 12:00:17 +0100

    Adjusted for new version

--- premake5.lua
+++ premake5.lua
@@ -7,11 +7,12 @@ project "ImGui"
 
     files
     {
-        "imconfig.h",
+		"imconfig.h",
         "imgui.h",
         "imgui.cpp",
         "imgui_draw.cpp",
         "imgui_internal.h",
+        "imgui_tables.cpp",
         "imgui_widgets.cpp",
         "imstb_rectpack.h",
         "imstb_textedit.h",
